eb6a17207e6e18ed434286ebd5f832337b54f14a
henriih18 <hernandezhenry58@gmail.com>
2024-10-10 11:47:37 -0500

    ajuste-2

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { ItemNavbar } from "../ItemNavbar/ItemNavbar";
 
 export const Navbar = () => {
-    return (
-        <nav>
-            <ul>
+  return (
+    <nav>
+      <ul>
+        <a href="#">
+          <ItemNavbar text="Quien soy" />
+        </a>
 
-                <ItemNavbar text="quien soy" />
-                <ItemNavbar text="mi familia" />
-                <ItemNavbar text="mis estudios" />
-            </ul>
-            
-        </nav>
-    )
-}
+        <a href="#">
+          <ItemNavbar text="Mi familia" />
+        </a>
+
+        <a href="#">
+          <ItemNavbar text="Mis estudios" />
+        </a>
+      </ul>
+    </nav>
+  );
+};
